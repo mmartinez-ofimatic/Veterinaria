6552997dbbea77441d4c174cf3947d5346e61098
mmartinez-ofimatic <mmartinez@ofimatic.com>
2014-06-17 11:12:30 -0400

    Proyecto

---
+++ Datos/Clientes_DAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Entidades;
+
+namespace Datos
+{
+    public class Clientes_DAL
+    {
+        public bool Guardar()
+        {
+            bool isComplete = false;
+            try
+            {
+
+            veterinariaDBEntities db = new veterinariaDBEntities();
+
+            Cliente clientes = new Cliente();
+            clientes.Cedula = Clientes_Entity.Cedula;
+            clientes.Nombre = Clientes_Entity.Nombre;        
+            clientes.Telefono = Clientes_Entity.Telefono;
+            clientes.Celular = Clientes_Entity.Celular;
+            clientes.Direccion = Clientes_Entity.Direccion;
+            
+
+            db.Clientes.AddObject(clientes);
+            db.SaveChanges();
+            isComplete = true;
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return isComplete;
+        
+        }
+    }
+}
---
+++ Entidades/Clientes_Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class Clientes_Entity
+    {
+        public static string Cedula { get; set; }
+        public static string Nombre { get; set; }
+        public static string Telefono { get; set; }
+        public static string Celular { get; set; }
+        public static string Direccion { get; set; }
+       // public static string Fecha_Creacion { get; set; }
+
+    }
+}
